8ce669cb083ac7a3d36680a682bbeb39142ade38
TonYamano77 <38879504+tonyamano24@users.noreply.github.com>
2025-10-16 21:39:24 +0700

    Add ESLint workflow

---
+++ TonYamanoGallery/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+}
